40f9c19a80657445fe03281fc872f901d70c83d6
Gustavo Brigante (een) <153044760+eensides@users.noreply.github.com>
2024-11-30 21:56:58 -0300

    Update youtube-api.js

--- js/youtube-api.js
+++ js/youtube-api.js
@@ -1,3 +1,5 @@
+// youtube-api.js
+
 async function checkYouTubeLiveStatus(channelId) {
     const response = await fetch(`https://www.googleapis.com/youtube/v3/search?channelId=${channelId}&eventType=live&part=snippet&key=YOUR_YOUTUBE_API_KEY`);
     const data = await response.json();
